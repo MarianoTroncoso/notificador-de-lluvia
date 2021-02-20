8b092efac9dc36be11d2870556592ae9fd5f5a96
Mariano <petotronco@gmail.com>
2021-02-20 16:06:03 -0300

    establecer cron

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const cron = require('node-cron')
 
 const prueba = () => {
-  console.log('hola')
+  console.log('eventual aviso de lluvia')
 }
 
+// 0 20 * * * <-- todos los días a las 20 hs 
 cron.schedule('* * * * * *', () => {
   prueba()
 })
