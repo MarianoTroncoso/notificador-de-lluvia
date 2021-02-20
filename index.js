d374dd14d1492e8739a8d0ada53ca4c1b52b2a7a
Mariano <petotronco@gmail.com>
2021-02-20 18:32:32 -0300

    poner cron igual a cada minuto

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { obtenerProbabilidades } = require('./consultarClima')
 const { enviarEmail } = require('./email')
 
 // 0 20 * * * <-- todos los días a las 20 hs 
-cron.schedule('0 20 * * *', () => {
+// cron.schedule('0 20 * * *', () => {
+cron.schedule('* * * * *', () => {
   obtenerProbabilidades().then( res => {
     
     if(res.length != 0){
