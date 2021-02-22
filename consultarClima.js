2ea353b76efe35a918d1831cc7f59311d690f705
Mariano <petotronco@gmail.com>
2021-02-22 16:52:59 -0300

    Agregar el parametro que faltaba para el launch de puppeteer

--- consultarClima.js
+++ consultarClima.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const obtenerProbabilidades = async () => {
 
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto('https://www.smn.gob.ar/');
 
